791c67aec6d0111391e8a28e89d7af7ff101d042
Muhan <dosxand@bk.ru>
2023-02-01 12:08:29 +0600

    week2

---
+++ week2/operators.py
@@ -0,0 +1,13 @@
+print(10*5)
+
+print(10/2)
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+if 5 != 20:
+    print("5 is not equal to 20")
+    
+if 4>3 or 4>5:
+    print("At least one of the statements is true")
