08dbd46886382b9912829c0ee238f01a571c1612
Muhan <dosxand@bk.ru>
2023-02-01 12:47:51 +0600

    week2

---
+++ week2/ifelse.py
@@ -0,0 +1,37 @@
+a = 50
+b = 10
+if a > b:
+
+  print("Hello World")
+  
+a =50
+b =10
+if a != b :
+    print("Hello World")
+
+a =50
+b =10
+if a == b :
+    print("No")
+else :
+    print("yes")
+    
+a = 50
+b = 10
+if a  == b:
+
+  print("1")
+elif a > b:
+
+  print("2")
+
+else :
+    print("3")
+
+c = 12
+d = -45    
+if a==b and c==d:
+    print("hello world")
+
+if a==b or c==d:
+    print("hello world")
