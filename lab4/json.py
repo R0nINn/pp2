9941ec52685327bb509c056986c7bdc56818dee5
Muhan <dosxand@bk.ru>
2023-02-22 19:13:23 +0600

    week4

--- lab4/json.py
+++ lab4/json.py
@@ -1,5 +1,14 @@
 import json
-o = open('sample_json.txt')
-
-data = json.load(o)
-print(data['imdata'][0]['l1PhysIf']['attributes']['autoNeg'])
+with open("sample_json.json", "r") as read_file:
+    data = json.load(read_file)
+print(data)
+print("""Interface Status
+================================================================================""")
+print("""DN                                             Description          Speed                      MTU """)
+for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
+    if i == 'dn':
+        print(k, end="                          ")
+    if i == "speed":
+        print(k, end="                                         ")
+    if i == "mtu":
+        print(k, end="                   ")
