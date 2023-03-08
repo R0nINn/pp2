12fba0a9cf2286bf09d7ee5f3a4fb81dc2791910
Muhan <dosxand@bk.ru>
2023-03-08 23:24:32 +0600

    week6

--- lab6/ newdirectory/buitin.py
+++ lab6/ newdirectory/buitin.py
@@ -36,7 +36,17 @@ def isPal(word):
 print(isPal(word))
     
 
+import math
+import time
 
+num = int(input())
+millisec = int(input())
+
+time.sleep(millisec / 1000)
+
+ans = math.sqrt(num)
+
+print(f"Square root of {num} after {millisec} milliseconds is {ans}.")
 
 
 x = (True, True, False)
